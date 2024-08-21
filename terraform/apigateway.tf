bcc927033f927da2a81f2b74b67ac26072abda48
flmngwllm <flmngwllm232@gmail.com>
2024-08-21 00:17:14 -0400

    edits

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -22,7 +22,7 @@ resource "aws_api_gateway_method" "method" {
 
 resource "aws_api_gateway_integration" "integration" {
   
-  http_method = aws_api_gateway_method.method.http_method
+  http_method = aws_api_gateway_method.method[each.key].http_method
   resource_id = aws_api_gateway_resource.resource.id
   rest_api_id = aws_api_gateway_rest_api.api.id
   type        = "AWS_PROXY"
