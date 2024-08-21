bb298f0159fc2b3462267c7163cd05a4f4cc7c57
flmngwllm <flmngwllm232@gmail.com>
2024-08-21 02:21:59 -0400

    adjusted source arn

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -6,7 +6,7 @@ resource "aws_api_gateway_rest_api" "api" {
 
 resource "aws_api_gateway_resource" "resource" {
   parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-  path_part   = " "
+  path_part   = ""
   rest_api_id = aws_api_gateway_rest_api.api.id
 }
 
