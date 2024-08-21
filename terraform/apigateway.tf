60b918486506fe9a80b09b6794cce54997287bbd
flmngwllm <flmngwllm232@gmail.com>
2024-08-21 13:03:46 -0400

    edits

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -6,7 +6,7 @@ resource "aws_api_gateway_rest_api" "api" {
 
 resource "aws_api_gateway_resource" "resource" {
   parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-  path_part   = ""
+  path_part   = "counter"
   rest_api_id = aws_api_gateway_rest_api.api.id
 }
 
