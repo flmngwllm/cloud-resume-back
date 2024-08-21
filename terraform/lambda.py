05ff3a7eaa06d2165b9e2560f05f2406d053a19b
flmngwllm <flmngwllm232@gmail.com>
2024-08-21 19:49:55 -0400

    updated lamda code

--- terraform/lambda.py
+++ terraform/lambda.py
@@ -1,4 +1,5 @@
 import boto3
+import json
 
 dynamodb = boto3.resource('dynamodb')
 
@@ -16,6 +17,22 @@ def lambda_handler(event, context):
    },
    ReturnValues="UPDATED_NEW"
   )
-  return response
+  
 
+  updated_count = response['Attributes']['visit_count']
+    
+    # Returning the visit_count in a format the JavaScript expects
+  return {
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
+        'body': {
+            'visit_count': updated_count
+        },
 
+        'body': json.dumps({
+            'visit_count': updated_count
+        })
+    }
