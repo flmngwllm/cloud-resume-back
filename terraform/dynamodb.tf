880597776a9eebe22981e796b8830361a360af81
flmngwllm <flmngwllm232@gmail.com>
2024-08-21 01:18:03 -0400

    removed read and write capacity

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,8 +1,6 @@
 resource "aws_dynamodb_table" "web_visit" {
   name           = "web_visitors"
   billing_mode   = "PAY_PER_REQUEST"
-  read_capacity  = 20
-  write_capacity = 20
   hash_key       = "visitor_id"
   
 
