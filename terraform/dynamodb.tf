7acaf04359039dbd0e295e01dd08eb08e2f78b6c
flmngwllm <flmngwllm232@gmail.com>
2024-08-21 20:40:22 -0400

    edits to key type

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -9,6 +9,11 @@ resource "aws_dynamodb_table" "web_visit" {
     type = "N"
   }
 
+  attribute {
+    name = "visit_count"
+    type = "N"
+  }
+
   ttl {
     attribute_name = "TimeToExist"
     enabled        = true
@@ -28,8 +33,8 @@ resource "aws_dynamodb_table_item" "web_count" {
 
   item = <<ITEM
 {
-  "visitor_id": {"N": 1},
-  "visit_count": {"N": 0} 
+  "visitor_id": {"N": "1"},
+  "visit_count": {"N": "0"} 
 }
 ITEM
 }
