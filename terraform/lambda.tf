2274c4b51db1be6176de7dca8bd230abeafb80c2
flmngwllm <flmngwllm232@gmail.com>
2024-08-23 20:58:43 -0400

    edits

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -20,7 +20,6 @@ resource "aws_iam_role" "iam_for_lambda" {
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
-
 resource "aws_iam_role_policy" "lambda_dynamodb_policy" {
   name   = "lambda_dynamodb_policy"
   role   = aws_iam_role.iam_for_lambda.id
